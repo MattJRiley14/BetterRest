00710c071a63908df0c28a7c995657e014be9bd1
Matt Riley <matthew.riley@barren.kyschools.us>
2024-10-22 01:18:39 -0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by BCCS 2022 on 10/22/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
